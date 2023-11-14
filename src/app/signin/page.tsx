12a7bf558fecc51e417fdf101cd2eab344247201
iniubongisemin <eugeneinie@gmail.com>
2023-11-14 21:44:57 +0100

    feat: modified components

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,29 +1,11 @@
-"use client"
-import React, { useState } from 'react'
+'use client'
+import React from 'react'
+import SignInForm from '../components/SignInForm'
 
 export default function page() {
-
-    const [inputVal, setInputVal] = useState('')
-
-    const handleChang = (evt:any) => {
-      setInputVal(evt.target.value)
-    } 
-
-return (
+  return (
     <div>
-        <div className="relative top-20 bg-">
-          <p className='text-3xl font-bold'>Sign In</p>
-          <p>Name</p>
-          <form>        
-            <input type="text" value={inputVal} onChange={handleChang} placeholder='Enter your name' className='hover:border-[#3498db] w-[100%] h-[35px]'/>        
-          </form>
-
-          <p>Email Address</p>
-          <form>
-            <input type="email" value={inputVal} onChange={handleChang} placeholder='Enter your email address' className='hover:border-[#3498db] w-[100%] h-[35px]'/>
-          </form>
-          <button type="submit" className='font-bold bg-[#3498db] rounded-md px-7 py-2 my-4 hover:text-black'>Submit</button>
-        </div>
+      <SignInForm />
     </div>
   )
 }
